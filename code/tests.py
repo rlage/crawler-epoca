d13af726ca06454ab724a87f61c9919e2cdfb936
Rafael Lage Tavares <rlt.rafael@gmail.com>
2017-02-13 20:35:54 -0200

    Preparando testes default e empty

--- code/tests.py
+++ code/tests.py
@@ -3,16 +3,40 @@
 import script
 import unittest
 
-class retrieveAllUrls(unittest.TestCase):
+class retrieveAllCategoryUrls(unittest.TestCase):
 
-    def testOne(self):
-        self.assertTrue(script.retrieveAllUrls(1))
+  def testEmpty(self):
+    self.assertTrue(script.retrieveAllCategoryUrls(1))
 
-    def testTwo(self):
-        self.assertFalse(script.retrieveAllUrls())
+  def testDefault(self):
+    self.assertFalse(script.retrieveAllCategoryUrls())
+
+class retrieveProductsFromCategory(unittest.TestCase):
+
+  def testEmpty(self):
+    self.assertTrue(script.retrieveProductsFromCategory(1))
+
+  def testDefault(self):
+    self.assertFalse(script.retrieveProductsFromCategory())
+
+class retrieveProducts(unittest.TestCase):
+
+  def testEmpty(self):
+    self.assertTrue(script.retrieveProducts(1))
+
+  def testDefault(self):
+    self.assertFalse(script.retrieveProducts())
+
+class writeInformationToCsv(unittest.TestCase):
+
+  def testEmpty(self):
+    self.assertTrue(script.writeInformationToCsv(1))
+
+  def testDefault(self):
+    self.assertFalse(script.writeInformationToCsv())
 
 def main():
-    unittest.main()
+  unittest.main()
 
 if __name__ == '__main__':
-    main()
+  main()
